d40ed52048a866747b3926e40963d18eea614a63
patxijuaristi <patxi585@gmail.com>
2023-01-22 13:23:49 +0100

    djang_extensions removed

--- app/settings/base.py
+++ app/settings/base.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     'fontawesomefree',
-    'django_extensions',
 ]
 
 MIDDLEWARE = [
